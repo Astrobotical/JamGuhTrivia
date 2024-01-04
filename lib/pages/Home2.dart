2aa0bcd2227af154c8d677df592894923828f786
neville-raheem <nevilleraheemdavis@gmail.com>
2024-01-03 21:16:54 -0500

    Updated select difficulty UI 1

--- lib/pages/Home2.dart
+++ lib/pages/Home2.dart
@@ -15,40 +15,93 @@ class MyHomePageTwo extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Image(
-                image: AssetImage(
-                    "lib/assets/images/jamaica_flag_brushStroke.png")),
-            const Text("Select Difficulty"),
-            ListView.builder(
+            const Padding(
+              padding: EdgeInsets.all(88.0),
+              child:  Image(
+                  image: AssetImage(
+                      "lib/assets/images/jamaica_flag_brushStroke.png")),
+            ),
+            const Text(
+              "Select Difficulty",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ListView.separated(
               shrinkWrap: true,
               itemCount: 3,
-              itemBuilder: (context, index) => SizedBox(
-                child: Row(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(
-                        children: [
-                          Text(
-                            "Beginner",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+              separatorBuilder: (context, index) => const Divider(color: Colors.grey),
+              itemBuilder: (context, index) {
+                List<Color> colorlist = [
+                  Colors.black,
+                  Colors.green,
+                  Colors.yellow
+                ];
+                List<String> difficulty = [
+                  "Beginner",
+                  "Intermediate",
+                  "Advanced"
+                ];
+                List<int> points = [2, 3, 5];
+
+                return SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 28.0),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            height: 50,
+                            width: 15,
+                            decoration: BoxDecoration(
+                              color: colorlist[index],
                             ),
                           ),
-                          Text("1 point per answer", style: TextStyle(color: Color.fromARGB(255, 102, 102, 102), fontSize: 12),)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              difficulty[index],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                  color: Color.fromARGB(255, 102, 102, 102),
+                                  fontSize: 12,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: points[index]
+                                        .toString(), // Bold and slightly larger
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize:
+                                          18, // Slightly larger than the rest
+                                    ),
+                                  ),
+                                  const TextSpan(
+                                      text: " points per correct answer"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       );
